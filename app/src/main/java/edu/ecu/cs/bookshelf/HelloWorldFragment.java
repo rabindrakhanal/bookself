43bdc7f83089ebc033d56b6c07a232d9b5a25f68
patelvishwa112 <31246787+patelvishwa112@users.noreply.github.com>
2017-11-30 05:11:19 -0500

    Final Navigation Bar

---
+++ app/src/main/java/edu/ecu/cs/bookshelf/HelloWorldFragment.java
@@ -0,0 +1,24 @@
+package edu.ecu.cs.bookshelf;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by patel on 11/30/2017.
+ */
+
+public class HelloWorldFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+}
