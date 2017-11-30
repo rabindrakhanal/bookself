bd8e8bf6f0808187e20879c7e68c198c7bb5525b
venkateshreddypala <venkateshreddypala@gmail.com>
2017-11-30 00:16:39 -0500

    Removed Google SignIn

--- app/src/main/java/edu/ecu/cs/bookshelf/LoginFragment.java
+++ app/src/main/java/edu/ecu/cs/bookshelf/LoginFragment.java
@@ -27,15 +27,14 @@ import java.util.HashMap;
 
 public class LoginFragment extends Fragment {
 
-    private static final int RC_SIGN_IN = 1;
-    private static final String TAG = "SignInActivity";
+
     private EditText mEmailAddress;
     private EditText mPassword;
     private Button mSubmitButton;
     private Button mSignUpButton;
 
     private HashMap<String, String> mUser = new HashMap<String, String>();
-    private GoogleSignInClient mGoogleSignInClient;
+
 
     public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
@@ -112,16 +111,9 @@ public class LoginFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = SignUpActivity.newIntent(getActivity());
                 startActivity(intent);
-                signIn();
+
             }
         });
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .requestProfile()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this.getActivity(), gso);
 
 
         return view;
@@ -129,59 +121,14 @@ public class LoginFragment extends Fragment {
 
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this.getActivity());
-        if (account != null) {
-            Toast.makeText(this.getActivity(), "Already Signed In!!", Toast.LENGTH_SHORT).show();
-       updateUI(account);
-        }
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
-    }
 
 
-    private void updateUI(GoogleSignInAccount account) {
-        if (account != null) {
-            //account.getDisplayName();
-            //account.getEmail();
-            String personName = account.getDisplayName();
-            String personEmail = account.getEmail();
 
 
-        } else {
-            mEmailAddress.setText(R.string.no_email_found);
 
-        }
-    }
+
+
+
+
+
 }
